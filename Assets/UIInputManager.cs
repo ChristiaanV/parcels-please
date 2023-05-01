82c819b1607eccb4672bc2d6c433e153bb6e7273
Christiaan <cvolschenk19@gmail.com>
2023-05-01 13:49:14 +0200

    Added music and sfx to settings menu

--- Assets/UIInputManager.cs
+++ Assets/UIInputManager.cs
@@ -2,15 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Slider = UnityEngine.UI.Slider;
 
 public class UIInputManager : MonoBehaviour
 {
     [SerializeField] private PlayerPrefsSO _playerPrefsSo;
+
+
+    [SerializeField] private Slider mouse, music, sfx;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        mouse.value = _playerPrefsSo.GetMouseSensitivity();
+        music.value = _playerPrefsSo.GetMusicVolume();
+        sfx.value = _playerPrefsSo.GetSFXVolume();
     }
 
     // Update is called once per frame
@@ -19,9 +26,20 @@ public class UIInputManager : MonoBehaviour
         
     }
 
+
     public void UpdateMouseSensitivity(float s)
     {
         _playerPrefsSo.SetMouseSensitivity(s);
+    }
+    
+    public void UpdateMusic(float s)
+    {
+        _playerPrefsSo.SetMusicVolume(s);
+    }
+    
+    public void UpdateSFX(float s)
+    {
+        _playerPrefsSo.SetSFXVolume(s);
     }
 
 }
