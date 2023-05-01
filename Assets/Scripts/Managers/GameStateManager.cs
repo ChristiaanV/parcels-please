1b1eb05e9b3ee03670817ee1a761d4da1969caee
Christiaan <cvolschenk19@gmail.com>
2023-05-01 22:35:46 +0200

    Changed settings key and moved QC sign

--- Assets/Scripts/Managers/GameStateManager.cs
+++ Assets/Scripts/Managers/GameStateManager.cs
@@ -18,7 +18,7 @@ public class GameStateManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Q))
         {
             InGameState = !InGameState;
             
