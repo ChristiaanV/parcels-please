6d320a9abb439556ca55ff68a7a83ffb694b0172
Christiaan <cvolschenk19@gmail.com>
2023-05-01 19:41:45 +0200

    Added gigacannon

---
+++ Assets/GigaCannonManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class GigaCannonManager : MonoBehaviour
+{
+    [SerializeField] private GameObject firePrompt;
+    [SerializeField] private CinemachineVirtualCamera gigaCam;
+
+    private bool ready = false;
+    
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (ready && Input.GetKeyDown(KeyCode.F))
+        {
+            firePrompt.SetActive(false);
+
+            ready = false;
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag != "Parcel") return;
+        
+        col.gameObject.SetActive(false);
+        
+        firePrompt.SetActive(true);
+        gigaCam.Priority = 100;
+        
+        ready = true;
+
+    }
+
+    private void FireGigaCannon()
+    {
+        
+    }
+}
